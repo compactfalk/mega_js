90861b306c9a8b9490cbba6384e3b498c2fd23d1
compactfalk <vstezh@gmail.com>
2023-07-14 21:57:39 +0600

    13.07 HW

---
+++ 13-07.js
@@ -0,0 +1,22 @@
+// //1, 2
+// let Name = 'Val';
+// let age = 22;
+// alert(`Hello, ${Name}!`);
+// alert(`I'm ${age} years old`);
+
+// //3
+// let str1 = "Salam, ";
+// let str2 = "Bishkek!";
+// alert(str1 + str2);
+
+// //4
+// let a = 17, b = 10;
+// let c = a -= b, d = 7;
+// let result = c += d;
+// alert(result);
+
+// //5
+// let hr = 2, min = 2, sec = 2;
+// alert(`${hr}:${min}:${sec}`);
+
+//6
