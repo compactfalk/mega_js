4d2eb3ad80c8de0f319b75ba17f5b06a5f206c7f
compactfalk <vstezh@gmail.com>
2023-07-15 07:26:41 +0600

    fix

--- 15.07.js
+++ 15.07.js
@@ -9,7 +9,19 @@ console.log(num1);
 //2
 
 let num2 = 47;
-num2 += 7 -= 18 *= 10 /=15;
+num2 += 7;
+num2 -= 18;
+num2 *= 10;
+num2 /= 15;
 alert(num2);
 
 //3
+
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
