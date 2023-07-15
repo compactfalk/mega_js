95b00fb3a953b70a9a50aecad666f212e8e4d765
compactfalk <vstezh@gmail.com>
2023-07-15 08:26:28 +0600

    huhuhu

--- 15.07.js
+++ 15.07.js
@@ -1,5 +1,4 @@
 //1
-
 let num1 = 10;
 num1++;
 num1++;
@@ -7,7 +6,6 @@ num1--;
 console.log(num1);
 
 //2
-
 let num2 = 47;
 num2 += 7;
 num2 -= 18;
@@ -16,7 +14,6 @@ num2 /= 15;
 alert(num2);
 
 //3
-
 let num = 1;
 num += 12;
 num -= 14;
