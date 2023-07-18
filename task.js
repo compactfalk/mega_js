04dd29d636e8f603119dada90ff31212b6d1a592
compactfalk <vstezh@gmail.com>
2023-07-18 20:52:39 +0600

    task.fixed

--- task.js
+++ task.js
@@ -1,12 +1,13 @@
 //1
-let = Number(promt('enter number'));
+let x = Number(prompt('enter number'));
 if (x == 7)
   alert('верно');
 else
   alert('не верно');
 
 //2
-let a = Number(promt('enter number'));
+let a = Number(prompt('enter value of a'));
+let b = Number(prompt('enter value of b'));
 if (a <= 1 && b >= 3)
   alert(a + b);
 else
