35d1b73e4b420236daa36c6917709b4b8ba53980
compactfalk <vstezh@gmail.com>
2023-07-18 20:41:14 +0600

    task

---
+++ task.js
@@ -0,0 +1,13 @@
+//1
+let = Number(promt('enter number'));
+if (x == 7)
+  alert('верно');
+else
+  alert('не верно');
+
+//2
+let a = Number(promt('enter number'));
+if (a <= 1 && b >= 3)
+  alert(a + b);
+else
+  alert(a - b);
