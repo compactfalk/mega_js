1bceeb4a1eeaa40b0fe18617ce8f1db029ec72bb
compactfalk <vstezh@gmail.com>
2023-07-31 23:41:54 +0600

    finally, folders!

---
+++ 1.08/script.js
@@ -0,0 +1,31 @@
+// /*1*/
+
+// let n = Number(prompt('enter length of array'));
+// let A = new Array(n);
+// for (let i = 0; i < n; i++)
+//     A[i] = Number(prompt(`enter value of ${i}th element`));
+// let A$ = A.map((x) => x*2);
+// console.log(A$);
+
+// /*2*/
+
+// let a = String(prompt('enter string'));
+// let n = Number(prompt('enter length of array'));
+// const F = (a, n) =>
+//  {
+//     let K = new Array(y);
+//     for (let i = 0; i < n; i++)
+//         K[i] = a;
+//     return K;
+//  }
+
+/*3*/
+
+// let a = Number(prompt('enter first number')); b = Number(prompt('enter second number'));
+// const Compare = (a, b) =>
+//  {
+//     let res;
+//     res = (a < b) ? -1 : (a == b ? 0 : 1);
+//     console.log(res);
+//     return res;
+//  }
