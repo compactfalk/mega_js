23b1b338c6fe9597c6d2114b4b275d011c17c685
compactfalk <vstezh@gmail.com>
2023-08-05 19:37:53 +0600

    h

--- 05.08/script.js
+++ 05.08/script.js
@@ -1,25 +1,59 @@
+// //1
+// let arr = new Array;
+// arr[0] = 0;
+// let cnt = 0;
+// arr.forEach(function (item, index, arr){
+//     if (item != 99)
+//      {
+//         cnt++;
+//         arr[index + 1] = Number(prompt());
+//      } else alert(`количество чисел: ${cnt}`);
+//  })
+
+// //2
+// let arr = new Array;
+// arr[0] = 0;
+// let cnt = 0;
+// let sum = 0;
+// arr.forEach(function (item, index, arr){
+//     if (item != 999)
+//     {
+//         cnt++;
+//         sum += item;
+//         arr[index + 1] = Number(prompt());
+//     } else alert(`количество чисел: ${cnt}, сумма: ${sum}`);
+// })
+// //3
+// function fromPairs(data) {
+//     let obj = {};
+
+// }
+
 //1
-let arr = new Array;
-arr[0] = 0;
-let cnt = 0;
-arr.forEach(function (item, index, arr){
-    if (item != 99)
-     {
+function NN() {
+    let cnt = 0;
+    let n = Number(prompt());
+    if (n != 99) {
         cnt++;
-        arr[index + 1] = Number(prompt());
-     } else alert(`количество чисел: ${cnt}`);
- })
+        NN();
+    } else {
+        console.log(cnt);
+        return 0;
+    }
+}
 
 //2
-let arr = new Array;
-arr[0] = 0;
-let cnt = 0;
-let sum = 0;
-arr.forEach(function (item, index, arr){
-    if (item != 999)
-    {
+function NNN() {
+    let cnt = 0;
+    let sum= 0;
+    let n = Number(prompt());
+    if (n != 999) {
         cnt++;
-        sum += item;
-        arr[index + 1] = Number(prompt());
-    } else alert(`количество чисел: ${cnt}, сумма: ${sum}`);
-})
+        sum += n;
+        NNN();
+    } else {
+        console.log(cnt);
+        console.log(sum);
+        return 0;
+    }
+}
